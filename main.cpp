da30b3a36ecee1bbd0e2a018e836c68eea59025f
yafeunteun <yannfeunteun@gmail.com>
2013-11-03 14:48:45 +0100

    Création du protocole de test du travail effectué à l'étape 6 du projet. voir main.cpp ou étape 6 du sujet de TP pour plus de détails.

--- main.cpp
+++ main.cpp
@@ -2,24 +2,45 @@
 #include "multimedia.h"
 #include "photo.h"
 #include "video.h"
+#include "film.h"
 
 using namespace std;
 
+// PROTOCOLE DE TEST DE L ETAPE 6 :
+
+// Implementez votre classe et vérifiez que le resultat est correct en détruisant le tableau passé en argument dans main()
+    // puis en appelant la fonction d'affichage de l'objet (NB: il faut répéter ces opérations pour plusieurs objets pour être sûr
+    // qu'il n'y a pas d'erreur à l'exécution).
+
+
 int main()
 {
+    Film* f1 = new Film("Pearl Harbor", 0, "/home/yafeunteun/Videos/ph.avi", 183*60 );
+    Film* f2 = new Film("Armageddon", 0, "/home/yafeunteun/Videos/argd.avi", 145*60 );
+    Film* f3 = new Film("Gladiator", 0, "/home/yafeunteun/Videos/gldr.avi", 155*60);
 
-    Multimedia *tab[4];
+    uint16_t* tabChap1 = new uint16_t[7];
+    uint16_t* tabChap2 = new uint16_t[7];
+    uint16_t* tabChap3 = new uint16_t[7];
 
-    tab[0] = new Photo("Fichier 1");
-    tab[1] = new Photo("Fichier 2");
-    tab[2] = new Video("Fichier 3");
-    tab[3] = new Video("Fichier 4");
-
-    for(unsigned char i = 0; i<4; ++i)
+    for(unsigned char i = 0; i < 7; ++i )
     {
-        tab[i]->affiche();
-        delete tab[i];
+        tabChap1[i] = 21;
+        tabChap2[i] = 15;
+        tabChap3[i] = 17;
     }
+
+    f1->setDureesChapitres(tabChap1, 7);
+    f2->setDureesChapitres(tabChap2, 7);
+    f3->setDureesChapitres(tabChap3, 7);
+
+    delete tabChap1;
+    delete tabChap2;
+    delete tabChap3;
+
+    f1->affiche();
+    f2->affiche();
+    f3->affiche();
 
 
     return 0;
